acadecf691f734b500378ba4eec7d673b366888f
Bogdan Kostiuk <delovoidagdan@gmail.com>
2025-12-18 15:45:49 +0200

    checking auto trigger

--- travel_dairy/lib/main.dart
+++ travel_dairy/lib/main.dart
@@ -51,7 +51,7 @@ class TravelDairyApp extends StatelessWidget {
     const primaryBlue = Color(0xFF1A56DB);
 
     return MaterialApp(
-      title: 'Travel Dev 🛠️',
+      title: 'Travel Dev auto 🛠️',
       debugShowCheckedModeBanner: false,
       // Localization setup
       localizationsDelegates: const [
